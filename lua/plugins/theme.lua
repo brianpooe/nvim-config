3f561e066738b0fb2aec97e29605dcd8301a7696
Brian Pooe <bria.method@gmail.com>
2023-12-21 15:49:26 +0200

    feat: orverride todo color style

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,9 +1,26 @@
 return {
-  { "brianpooe/darcula-solid.nvim", requires = "rktjmp/lush.nvim" },
+  -- { "brianpooe/darcula-solid.nvim", requires = "rktjmp/lush.nvim" },
+  -- { "rebelot/kanagawa.nvim" },
+  {
+    "AlexvZyl/nordic.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      local C = require("nordic.colors")
+      require("nordic").setup({
+        override = {
+          ["@lsp.type.interface"] = { link = "Type" },
+          TodoBgTODO = { link = "Function" },
+          TodoFgTODO = { link = "Function" },
+        },
+      })
+      require("nordic").load()
+    end,
+  },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "darcula-solid",
+      colorscheme = "nordic",
       priority = 1000,
     },
   },
